b2c6d7178b2b3d8da91294a7d8a68101f5071d9e
Anton Keks <anton@azib.net>
2016-06-06 23:11:24 +0300

    add collections

--- src/intro/advanced/2-extensions.kt
+++ src/intro/advanced/2-extensions.kt
@@ -4,6 +4,4 @@ fun List<String>.hello() = print(this)
 
 fun main(args: Array<String>) {
   listOf("a", "b").hello()
-
-  listOf(1, 2, 3).filter { it > 1 }.map { it.toString() }.groupBy { it.length }
 }
---
+++ src/intro/advanced/3-collections.kt
@@ -0,0 +1,11 @@
+package intro.advanced
+
+fun main(args: Array<String>) {
+  val list = mutableListOf(1, 2, 3)
+  println(list is kotlin.collections.List<*>)
+  println(list is java.util.List<*>)
+
+  // list.add(1) - impossible, use mutableListOf()
+
+  list.filter { it > 1 }.map { it.toString() }.groupBy { it.length }
+}
