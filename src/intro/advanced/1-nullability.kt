5f9dff35dc1bbc9d55686285ad8de803e10e7194
Anton Keks <anton@azib.net>
2016-06-06 22:31:13 +0300

    add nullable operators

--- src/intro/advanced/1-nullability.kt
+++ src/intro/advanced/1-nullability.kt
@@ -3,4 +3,8 @@ package intro.advanced
 fun main(args: Array<String>) {
   var notNull: String = ""
   var nullable: String? = null
+
+  println(notNull.length)
+  println(nullable?.length)
+  println(nullable ?: "hello")
 }
