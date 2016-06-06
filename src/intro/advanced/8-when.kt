d947842793db6d8974484fe14fc50a1d708f1b89
Anton Keks <anton@azib.net>
2016-06-07 00:18:54 +0300

    show that toString() is null-safe

--- src/intro/advanced/8-when.kt
+++ src/intro/advanced/8-when.kt
@@ -3,13 +3,14 @@ package intro.advanced
 data class Person(val name: String)
 
 fun main(args: Array<String>) {
-  val x: Any = ""
+  val x: Any? = null
   when (x) {
     "" -> println("empty String")
     is String -> println("a String")
     0, 1 -> print("0 or 1")
     in 2..10 -> print("from 2 to 10")
     Person("Anton") -> print("is Anton")
+    x.toString().startsWith("A") -> print("starts with A")
     else -> print("nothing else matched")
   }
 }
