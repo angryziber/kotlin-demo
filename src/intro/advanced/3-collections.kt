94e35719ddad4c09669034fbbaf8c0fb5aec7536
Anton Keks <anton@azib.net>
2016-06-08 18:15:32 +0300

    use listOf(), so adding is really impossible

--- src/intro/advanced/3-collections.kt
+++ src/intro/advanced/3-collections.kt
@@ -1,11 +1,12 @@
 package intro.advanced
 
 fun main(args: Array<String>) {
-  val list = mutableListOf(1, 2, 3)
+  val list = listOf(1, 2, 3)
   println(list is kotlin.collections.List<*>)
   println(list is java.util.List<*>)
 
-  // list.add(1) - impossible, use mutableListOf()
+  // impossible, use mutableListOf():
+  // list.add(1)
 
   list.filter { it > 1 }.map { it.toString() }.sortedBy { it.hashCode() }
 }
