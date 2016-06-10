4052a8c7221efee0da6675fff77587ab9d14ca22
Anton Keks <anton@azib.net>
2016-06-11 00:29:55 +0300

    add comments on how to get mockito working

--- test/RawMockitoTest.kt
+++ test/RawMockitoTest.kt
@@ -7,15 +7,18 @@ import org.mockito.Mockito.mock
 import kotlin.test.assertEquals
 
 open class RawMockitoTest {
-  class KotlinClass {
-    fun mockMe(s: String): String = s
+  // mockito can only mock open classes and methods
+  open class KotlinClass {
+    open fun mockMe(s: String): String = s
   }
 
   @Test @Ignore("Doesn't work with non-open class")
   fun rawMockito() {
     val o = mock(KotlinClass::class.java)
 
-    `when`(o.mockMe(any(String::class.java))).thenReturn("hello")
+    // `when` is a reserved word in Kotlin
+    // Mockito matchers return nulls, which fail Kotlin's runtime null checks
+    `when`(o.mockMe(any(String::class.java) ?: "")).thenReturn("hello")
 
     assertEquals("hello", o.mockMe("any string"));
   }
