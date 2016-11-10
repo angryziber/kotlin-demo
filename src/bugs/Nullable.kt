23ccc7f2fe94b48e72112532a9d0eb9eefb5f385
Anton Keks <anton@azib.net>
2016-11-10 17:12:22 +0200

    Kotlin compiler is finally fixed for this case

--- src/bugs/Nullable.kt
+++ src/bugs/Nullable.kt
@@ -3,7 +3,7 @@ package bugs
 fun parse(text: String) = text.split(";\\s*".toRegex())
   .map { it.split("=", limit=2) }
   .associate {
-    if (it.size < 2) return null
+    // if (it.size < 2) return null - this one is fixed in compiler, and doesn't compile anymore
     it[0].trim() to it[1]
   }
 
