bc734ea4848a00057ebaa491e77f09bc8bf0ffc6
Anton Keks <anton@azib.net>
2018-11-28 00:17:31 +0200

    use canonical order

--- src/intro/advanced/8-operators.kt
+++ src/intro/advanced/8-operators.kt
@@ -1,8 +1,8 @@
 package intro.advanced
 
 data class Complex(val r: Double, val i: Double) {
-  operator infix fun plus(that: Complex) = Complex(r + that.r, i + that.i)
-  operator infix fun minus(that: Complex) = Complex(r - that.r, i - that.i)
+  infix operator fun plus(that: Complex) = Complex(r + that.r, i + that.i)
+  infix operator fun minus(that: Complex) = Complex(r - that.r, i - that.i)
 }
 
 fun main(args: Array<String>) {
