17f46609948722b2aace3d7646e5273ded0bfc10
Anton Keks <anton@azib.net>
2018-11-29 19:01:23 +0200

    keep vars to demonstrate assignment

--- src/intro/advanced/1-nullability.kt
+++ src/intro/advanced/1-nullability.kt
@@ -1,8 +1,8 @@
 package intro.advanced
 
 fun main() {
-  val notNull: String = ""
-  val nullable: String? = null
+  var notNull: String = ""
+  var nullable: String? = null
 
   println(notNull.length)
   println(nullable?.length)
